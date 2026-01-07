f04a8423f368d9d95a02811870db2fa1bf3dcd70
Jamer Delgado <133704939+Jd2698@users.noreply.github.com>
2026-01-06 20:33:09 -0500

    add envs.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as dotenv from 'dotenv';
-dotenv.config();
+import { envs } from '@config/envs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.port || 3000);
 }
 bootstrap();
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,12 +2,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma-generated/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { envs } from '@config/envs';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
 
   constructor() {
-    const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+    const pool = new PrismaPg({ connectionString: envs.databaseUrl! });
     super({ adapter: pool });
   }
 }
